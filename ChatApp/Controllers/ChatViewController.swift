e64b47563fb1a9d072ff6d0869dd8890d2c43b57
Atheer1423 <Atheer-ksa-2@hotmail.com>
2022-01-05 19:24:44 +0000

    ..

--- ChatApp/Controllers/ChatViewController.swift
+++ ChatApp/Controllers/ChatViewController.swift
@@ -8,24 +8,25 @@
 import UIKit
 import MessageKit
 import InputBarAccessoryView
+import GoogleUtilities_UserDefaults
 
 struct Message : MessageType {
-    var sender: SenderType
+   public var sender: SenderType
     
-    var messageId: String
+    public var messageId: String
     
-    var sentDate: Date
+    public var sentDate: Date
     
-    var kind: MessageKind
+    public var kind: MessageKind
 
 }
 
 struct Senderstruct : SenderType {
-    var senderId: String
+    public var senderId: String
     
-    var displayName: String
+    public var displayName: String
     
-    var photurl: String
+    public var photurl: String
     
 }
 extension MessageKind {
@@ -56,104 +57,74 @@ extension MessageKind {
 }
 
 class ChatViewController: MessagesViewController {
-//
-//    public static var dateFormatter: DateFormatter = {
-//           let formatter = DateFormatter()
-//           formatter.dateStyle = .medium
-//           formatter.timeStyle = .long
-//           formatter.locale = .current
-//           return formatter
-//       }()
-//
-//    public let otherUserEmail: String
-//       private let conversationId: String?
-//       public var isNewConversation = false
-//
+    
+    public static var dateFormatter: DateFormatter = {
+           let formatter = DateFormatter()
+           formatter.dateStyle = .medium
+           formatter.timeStyle = .long
+           formatter.locale = .current
+           return formatter
+       }()
+       
+    public var isNewConversation = false
+    public let otherUserEmail: String
     private var messages : [Message] = []
-    private var sender = Senderstruct(senderId: "1", displayName: "sara", photurl: "")
-//
-//    required init?(coder: NSCoder) {
-////        fatalError("init(coder:) has not been implemented"
-//    }
-//
-//    init(with email: String, id: String?) {
-//           self.conversationId = id
-//           self.otherUserEmail = email
-//        super.init(nibName: nil, bundle: nil)
-//
-//           // creating a new conversation, there is no identifier
-//
-//       }
-//
-//
-//
-//
+    
+    private var selfSender : Senderstruct? {
+        guard let email =   UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        
+   return Senderstruct(senderId:email ,displayName: "sara", photurl: "")
+        
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(with email: String) {
+
+           self.otherUserEmail = email
+        super.init(nibName: nil, bundle: nil)
+
+           // creating a new conversation, there is no identifier
+
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-        messages.append(Message(sender: sender, messageId: "1", sentDate: Date(), kind: .text("hello")))
-        messages.append(Message(sender: sender, messageId: "1", sentDate: Date(), kind: .text("hi")))
-        messages.append(Message(sender: sender, messageId: "1", sentDate: Date(), kind: .text("how are you")))
+    
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDataSource = self
         messagesCollectionView.messagesDisplayDelegate = self
         messageInputBar.delegate = self
+     
 
 
     }
-//
+    
+    
+
     override func viewDidAppear(_ animated: Bool) {
            super.viewDidAppear(animated)
-           messageInputBar.inputTextView.becomeFirstResponder()
+           messageInputBar.inputTextView.becomeFirstResponder() // present keyboard
 
-//           if let conversationId = conversationId {
-//               listenForMessages(id:conversationId, shouldScrollToBottom: true)
-//           }
        }
-//
-//
-//    private func listenForMessages(id: String, shouldScrollToBottom: Bool) {
-//
-////            DatabaseManger.shared.getAllMessagesForConversation(with: id) { [weak self] result in
-////                switch result {
-////                case .success(let messages):
-////                    print("success in getting messages: \(messages)")
-////                    guard !messages.isEmpty else {
-////                        print("messages are empty")
-////                        return
-////                    }
-////                    self?.messages = messages
-////
-////                    DispatchQueue.main.async {
-////                        self?.messagesCollectionView.reloadDataAndKeepOffset()
-////
-////                        if shouldScrollToBottom {
-////                            self?.messagesCollectionView.scrollToLastItem()
-////
-////                        }
-////
-////                    }
-////
-////                case .failure(let error):
-////                    print("failed to get messages: \(error)")
-////                }
-////            }
-//
     
+ 
 
 }
+
 extension ChatViewController :  MessagesDataSource,MessagesLayoutDelegate,MessagesDisplayDelegate {
     
-//    if let sender = selfSender {
-//
-//    return sender
-//       }
-//       print("Self sender is nil, email should be cached")
-        
-//       return Senderstruct(photoURL: "", senderId: "12", displayName: "")
-
     func currentSender() -> SenderType {
-        return sender
+        if let Sender = selfSender  {
+            return Sender
+        }
+        fatalError("self sender is nil ,email should be cached")
+        return Senderstruct(senderId: "123", displayName: "", photurl: "")
     }
     
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
@@ -166,67 +137,62 @@ extension ChatViewController :  MessagesDataSource,MessagesLayoutDelegate,Messag
 }
 
 extension ChatViewController: InputBarAccessoryViewDelegate {
-//    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
-//        guard !text.replacingOccurrences(of: " ", with: "").isEmpty, let selfSender = self.selfSender, let messageId = createMessageId()  else {
-//            return
-//        }
+    
+  
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        guard !text.replacingOccurrences(of: " ", with: "").isEmpty,
+        let selfSender = self.selfSender,
+        let messageId = createMessageId() else{
+            return
+        }
+    
         
-//        print("sending \(text)")
-//
-//        let message = Message(sender: selfSender, messageId: messageId, sentDate: Date(), kind: .text(text))
-//
-//        // Send message
-//        if isNewConversation {
-            // create convo in database
-            // message ID should be a unique ID for the given message, unique for all the message
-            // use random string or random number
-//            DatabaseManger.shared.createNewConversation(with: otherUserEmail, name: self.title ?? "User", firstMessage: message) { [weak self] success in
-//                if success {
-//                    print("message sent")
-//                    self?.isNewConversation = false
-//                }else{
-//                    print("failed to send")
-//                }
-//            }
-//
-//        }else {
-//            guard let conversationId = conversationId, let name = self.title else {
-//                return
-//            }
-            
-            // append to existing conversation data
-//            DatabaseManger.shared.sendMessage(to: conversationId, name: name, newMessage: message) { success in
-//                if success {
-//                    print("message sent")
-//                }else {
-//                    print("failed to send")
-//                }
-//            }
+        print("sending \(text)")
+        
+        // Send message
+   
+        if self.isNewConversation {
+            //crete convo in db
+            let message = Message(sender: selfSender, messageId: messageId, sentDate: Date(), kind: .text(text))
+            DatabaseManger.shared.createNewConversation(with: otherUserEmail, firstMessage: message, completion: {[weak self] success in
+                if success {
+                    print("message sent")
+                }else{
+                    print("failed to  sent")
+                }
+                
+            })
+        }else{
             
         }
-        
+    }
     
     private func createMessageId() -> String? {
         // date, otherUserEmail, senderEmail, randomInt possibly
         // capital Self because its static
-    
-        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String else {
+        let dateString = Self.dateFormatter.string(from: Date())
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String
+      else {
             return nil
         }
+        let safeCurentEmail = DatabaseManger.safeEmail(emailAddress: currentUserEmail)
         
-//        let safeCurrentEmail = DatabaseManger.safeEmail(emailAddress: currentUserEmail)
+       
+        let newIdentifier = " \(otherUserEmail)_\(safeCurentEmail)_\(dateString)"
+print("created message id : \(newIdentifier)")
         
-//        let dateString = Self.dateFormatter.string(from: Date())
-//        let newIdentifier = "\(otherUserEmail)_\(safeCurrentEmail)_\(dateString)"
-        
-    
-//        print("created message id: \(newIdentifier)")
-//        return newIdentifier
-        return " "
+        return newIdentifier
         
     }
 
 
 
-    
 
+
+
+            
+        }
+        
+        
+    
+  
--- ChatApp/Controllers/ConversationViewController.swift
+++ ChatApp/Controllers/ConversationViewController.swift
@@ -10,6 +10,9 @@ import FirebaseAuth
 import JGProgressHUD
 
 class ConversationViewController: UIViewController {
+    
+   
+    
     private let spinner = JGProgressHUD(style: .dark)
         
         private let tableView: UITableView = {
@@ -47,9 +50,26 @@ class ConversationViewController: UIViewController {
            // present new conversation view controller
            // present in a nav controller
         let vc = NewConversationViewController()
+        vc.completion = { [weak self]
+            result in
+//            print ("\(result)")
+            self?.createNewConversation(result: result)
+        }
                let navVC = UINavigationController(rootViewController: vc)
                present(navVC,animated: true)
        }
+    
+    private func createNewConversation(result:[String:String]){
+        guard let name = result["name"], let email = result["email"] else {
+            return
+        }
+        let ChatVC = ChatViewController(with:email)
+        ChatVC.isNewConversation = true
+        ChatVC.title =  name
+        ChatVC.navigationItem.largeTitleDisplayMode = .never
+        self.navigationController?.pushViewController(ChatVC, animated: true)
+
+    }
     
     private func fetchConversations(){
             // fetch from firebase and either show table or label
@@ -105,7 +125,7 @@ class ConversationViewController: UIViewController {
         func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             tableView.deselectRow(at: indexPath, animated: true)
 
-            let ChatVC = ChatViewController()
+            let ChatVC = ChatViewController(with: "ath@gmail.com")
             ChatVC.title =  "Atheer"
             ChatVC.navigationItem.largeTitleDisplayMode = .never
             self.navigationController?.pushViewController(ChatVC, animated: true)
